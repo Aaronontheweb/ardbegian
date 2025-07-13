3d1057a74fced4936114e13111b0c77fb677f34d
Aaron Stannard <aaron@petabridge.com>
2025-07-13 15:57:00 -0500

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -e
 
 ascii_art='________                  __        ___.
@@ -10,18 +11,17 @@ ascii_art='________                  __        ___.
 
 echo -e "$ascii_art"
 echo "=> Omakub is for fresh Ubuntu 24.04+ installations only!"
-echo -e "\nBegin installation (or abort with ctrl+c)..."
+echo -e "\nBegin installation (or abort with Ctrl+C)…"
 
-sudo apt-get update >/dev/null
-sudo apt-get install -y git >/dev/null
+# Make sure Git is present
+sudo apt-get update -qq
+sudo apt-get install -y git -qq
 
-echo "Cloning Omakub..."
+echo "Cloning Omakub (master branch)…"
 rm -rf ~/.local/share/omakub
-git clone https://github.com/Aaronontheweb/ardbegian.git ~/.local/share/omakub >/dev/null
-if [[ $OMAKUB_REF != "master" ]]; then
-	cd ~/.local/share/omakub
-	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
-	cd -
-fi
+git clone --depth 1 --branch master \
+  https://github.com/Aaronontheweb/ardbegian.git \
+  ~/.local/share/omakub >/dev/null
 
-echo "When you're ready to install, run: `source ~/.local/share/omakub/install.sh`"
+echo -e "\nWhen you’re ready to install, run:"
+echo "  source ~/.local/share/omakub/install.sh"
