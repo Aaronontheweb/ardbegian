050c8ca22d241d4334376bce48a725297d0de281
Aaron Stannard <aaron@petabridge.com>
2025-07-13 19:46:15 -0500

    Desktop: make JetBrains toolbox available (#20)

---
+++ uninstall/app-jetbrains-toolbox.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove JetBrains Toolbox installation
+sudo rm -rf /opt/jetbrains-toolbox
+
+# Remove user data (optional - uncomment if you want to remove all data)
+# rm -rf ~/.local/share/JetBrains
+# rm -rf ~/.config/JetBrains
+
+echo "JetBrains Toolbox uninstalled successfully!" 
